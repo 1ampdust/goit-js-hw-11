eb233f1c134d06c5de139844a71a4adbc391858f
Lampdust <lampdust06@gmail.com>
2023-12-18 23:55:19 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -118,3 +118,5 @@ function check(current, total) {
   Notiflix.Notify.success(`Hooray! We found ${currentSum} of ${total} images`);
   loadMore.style.display = 'block';
 }
+
+//я почав робити для кожної функції окремий файл, але щось не вийшло(
